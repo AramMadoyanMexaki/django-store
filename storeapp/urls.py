d8d26f457109daef516a60e6581ea0904fe917fd
Aram <arma-arman@mail.ru>
2024-08-27 00:36:52 +0400

    Added two pages 'Fruits' & 'Vegetables'

--- storeapp/urls.py
+++ storeapp/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path
 from storeapp import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 
 urlpatterns = [
     path('', views.index, name="home"),
-    path('fruits/', views.fruits, name="fruits"),
-    path('base/', views.base, name="base"),
-]
+    path('fruits/', views.fruits_page, name="fruits"),
+    path('vegetables/', views.vegetables_page, name="vegan"),
+    path('meat/', views.meat, name="meat"),
+    path('add_product/', views.add_product, name="add"),
+    path('login/', views.login, name="login"),
+    path('register/', views.register, name="register")
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- storeapp/views.py
+++ storeapp/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from django.shortcuts import redirect
+from django.http import HttpResponse, Http404
+from django.shortcuts import redirect, get_list_or_404
 from .models import *
 
 
@@ -18,9 +18,36 @@ def index(request):
 
     return render(request, "home.html", context)
 
-def fruits(request):
-    return HttpResponse("fruits")
 
-def base(request):
-    return render(request, "base.html", {})
+def fruits_page(request):
+    try:
+        fruits = Product.objects.filter(category__name="Fruits")
+    except Product.DoesNotExist:
+        raise Http404("No Product matches the given query.")
 
+    return render(request, "fruits_page.html", {"fruits": fruits})
+
+
+def vegetables_page(request):
+    try:
+        vegetables = Product.objects.filter(category__name="Vegetables")
+    except Product.DoesNotExist:
+        raise Http404("No Product matches the given query.")
+    
+    return render(request, "vegan.html", {"vegetables": vegetables})
+
+
+def add_product(request):
+    pass
+
+
+def login(request):
+    pass
+
+
+def meat(request):
+    pass
+
+
+def register(request):
+    pass
